d62897c4592f4062f3e41bbe91fbde26ea61b225
cauaoli-silva <cauafoxy@gmail.com>
2026-06-27 20:49:18 -0300

    atv 6 t3

--- src/test/java/JogoDaVelhaTest.java
+++ src/test/java/JogoDaVelhaTest.java
@@ -5,20 +5,15 @@ public class JogoDaVelhaTest {
 
     @Test
     public void testeCriarTabuleiro() {
-
         Tabuleiro tabuleiro = new Tabuleiro();
-
         assertNotNull(tabuleiro);
     }
 
     @Test
     public void testeCasaLivre() {
-
         Tabuleiro tabuleiro = new Tabuleiro();
 
-        // Casa vazia
         assertTrue(tabuleiro.casaLivre("A3"));
-
-        // Casa ocupada
         assertFalse(tabuleiro.casaLivre("A1"));
     }
+}
